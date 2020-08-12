95f9ff942d2715d8b8599d6db7faf6caefb45177
Mark Force <force.j.mark@gmail.com>
2020-08-12 11:31:23 -0400

    updates

--- app.js
+++ app.js
@@ -31,16 +31,64 @@
   /*
 on first render, load start page
 on click start, load question 1,
-initialize for loop to keep track of page
+initialize for loop to keep track of page number and return to display at question-index
 on click submit, take input 
 put input value in array
 increase i by 1
 go to page with index = i
 when i = 4
-on click submit, take input 
+on click submit, take input
 put input value in array
 take arr, parse into other arrays based on values (for loop?)
 see which array.length is longest
 load result page based on which array.length was longest
 on click start over, goes to start page
+
   */
+ 
+ function countIndex() {
+   let count = results.length;
+   return count;
+ }
+
+  function generateStart() {
+     $('main').html( `<div class='page'>
+     <button id="submit" class='btn'>Start Quiz</button>
+   </div>`)
+  }
+
+function handlePageTurn() {
+   // e.preventDefault();
+   $('main').on('click', '.btn', function (event)  {
+     generateNextQuestion();
+  });
+}
+
+  function generateNextQuestion() {
+     $('main').html(`<div class='page-instance'>
+     <div class='question-index'>${countIndex()}/5</div>
+     <div class="question-text">  </div>
+     <div class="answers">
+       <form id='answers-form'> 
+         <input type='radio' id='erin' name='answer' value='e'>
+         <label for="erin">Run and get Misaka</label><br>
+         <input type="radio" id="misaka" name="answer" value="m">
+         <label for="misaka">Herd the civilians to safety</label><br>
+         <input type="radio" id="titan" name="answer" value="t">
+         <label for="titan">I am the titan</label><br>
+         <input type="radio" id="levi" name="answer" value="l">
+         <label for="levi">Charge the titan horde</label><br>
+         <button id="submit" class='submit-btn btn'>Submit</button>
+       </form>
+     </div>
+   </div>`)
+  }
+
+  function main(){
+      generateStart();
+      handlePageTurn();
+  }
+
+
+
+  $(main());

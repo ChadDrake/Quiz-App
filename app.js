4cd44173e2aeefb758c0946920b69deb835f4e4a
Mark Force <force.j.mark@gmail.com>
2020-08-18 18:37:11 -0400

    added finishing touches for graders

--- app.js
+++ app.js
@@ -47,6 +47,11 @@ on click start over, goes to start page
 
   */
 
+//on click of titan answer display modal
+//on click of rest of answers display corresponding modal
+//use getFinalResults to display the current final answer
+//
+
 function countIndex() {
   let count = STORE[5].length;
   return count;
@@ -58,8 +63,88 @@ function generateStart() {
    </div>`);
 }
 
-function handlePageTurn() {
+function handleStart() {
   $('main').on('click', '.start', function (event) {
+    console.log('start button clicked')
+    $('main').html(`<div class='page-instance'>
+    <div class='question-index'>${countIndex()}/5</div>
+    <div class="question-text">${STORE[countIndex() - 1].question}</div>
+    <div class="answers">
+      <form id='answers-form' class='form'> 
+        <input type='radio' id='erin' name='answer' value='e' required>
+        <label for="erin">${STORE[countIndex() - 1].answerE}</label><br>
+        <input type="radio" id="misaka" name="answer" value="m">
+        <label for="misaka">${STORE[countIndex() - 1].answerM}</label><br>
+        <input type="radio" id="titan" name="answer" value="t">
+        <label for="titan">${STORE[countIndex() - 1].answerT}</label><br>
+        <input type="radio" id="levi" name="answer" value="l">
+        <label for="levi">${STORE[countIndex() - 1].answerL}</label><br>
+        <button type="submit" id="submit" class='submit-btn btn'>Submit</button>
+        <button type="button" id="next" class="next">Next</button>
+      </form>
+    </div>
+  </div>
+  <div id="answerEren" class="modal">
+    <!-- Modal content -->
+  <div class="modalEren">
+    <span class="close">&times;</span>
+    <p>That sounds like something Eren would do...</p>
+  </div>
+
+</div>
+<div id="answerMikasa" class="modal">
+    <!-- Modal content -->
+  <div class="modalMikasa">
+    <span class="close">&times;</span>
+    <p>That sounds like something Mikasa would do...</p>
+  </div>
+
+</div>
+<div id="answerTitan" class="modal">
+    <!-- Modal content -->
+  <div class="modalTitan">
+    <span class="close">&times;</span>
+    <p>It's looking like you need some serious help...</p>
+  </div>
+
+</div>
+<div id="answerLevi" class="modal">
+    <!-- Modal content -->
+  <div class="modalLevi">
+    <span class="close">&times;</span>
+    <p>That sounds like something Levi would do...</p>
+  </div>
+
+</div>`);
+  });
+}
+
+function generateModal(){
+  if($('input[name=answer]:checked').val() === 'e') {
+    answerEren.style.display = "block"
+  }
+  else if($('input[name=answer]:checked').val() === 'm') {
+    answerMikasa.style.display = "block"
+  }
+  else if($('input[name=answer]:checked').val() === 't') {
+    answerTitan.style.display = "block"
+  }
+  else if($('input[name=answer]:checked').val() === 'l') {
+    answerLevi.style.display = "block"
+}
+}
+
+function modalExit() {
+  $('main').click('span', function(){
+    answerEren.style.display = "none";
+    answerMikasa.style.display = "none";
+    answerTitan.style.display = "none";
+    answerLevi.style.display = "none";
+  })
+}
+
+function handlePageTurn() {
+  $('main').on('click', '.next', function (event) {
     generateNextQuestion();
   });
 
@@ -76,7 +161,8 @@ function handleAnswer() {
   $('main').on('submit', function (e) {
     e.preventDefault();
     STORE[5].push($('input[name=answer]:checked').val());
-    generateNextQuestion();
+    generateModal();
+
   });
 }
 
@@ -122,6 +208,7 @@ function getFinalResult() {
   let finalResult = (getResult(STORE[5]));
   return finalResult;
 }
+
 function finalResultEren() {
   $('main').html(`<div class='page-instance'>
   <div class='results'>
@@ -166,7 +253,7 @@ function finalResultLevi() {
         <div class='results'>
           <h2>Congrats! You're most similar to Levi</h2>
           <div class="character">
-            <p>Okay Mr. Badass, you're a better leader than anything else but your harshe edge can be offputting</p>
+            <p>Okay Mr. Badass, you're a better leader than anything else but your harsh edge can be offputting</p>
           </div>
           <div class="reset-state">
             <button id="reset-btn" class="reset-btn btn">Start over!</button>
@@ -174,10 +261,30 @@ function finalResultLevi() {
         </div>
       </div>`);
 }
+
+function currentScore(){
+  let score = '';
+  console.log('test')
+  if (getFinalResult() === 'erinFinal') {
+    console.log('you chose erin')
+    score = 'Eren';
+  }
+  else if (getFinalResult(STORE[5]) === "misakaFinal") {
+    score = 'Mikasa';
+  }
+  else if (getFinalResult(STORE[5]) === 'titanFinal') {
+    score = 'A Titan';
+  }
+  else if (getFinalResult(STORE[5]) === 'leviFinal') {
+    score = 'Levi';
+  }
+  return score;
+}
+
 function generateNextQuestion() {
   if (countIndex() <= 5) {
     $('main').html(`<div class='page-instance'>
-      <div class='question-index'>${countIndex()}/5</div>
+      <div class='question-index'>${countIndex()}/5 You're looking like ${currentScore()}</div>
       <div class="question-text">${STORE[countIndex() - 1].question}</div>
       <div class="answers">
         <form id='answers-form' class='form'> 
@@ -190,9 +297,42 @@ function generateNextQuestion() {
           <input type="radio" id="levi" name="answer" value="l">
           <label for="levi">${STORE[countIndex() - 1].answerL}</label><br>
           <button type="submit" id="submit" class='submit-btn btn'>Submit</button>
+          <button type="button" id="next" class="next">Next</button>
         </form>
       </div>
-    </div>`);
+    </div>
+    <div id="answerEren" class="modal">
+      <!-- Modal content -->
+    <div class="modalEren">
+      <span class="close">&times;</span>
+      <p>That sounds like something Eren would do...</p>
+    </div>
+  
+  </div>
+  <div id="answerMikasa" class="modal">
+      <!-- Modal content -->
+    <div class="modalMikasa">
+      <span class="close">&times;</span>
+      <p>That sounds like something Mikasa would do...</p>
+    </div>
+  
+  </div>
+  <div id="answerTitan" class="modal">
+      <!-- Modal content -->
+    <div class="modalTitan">
+      <span class="close">&times;</span>
+      <p>It's looking like you need some serious help...</p>
+    </div>
+  
+  </div>
+  <div id="answerLevi" class="modal">
+      <!-- Modal content -->
+    <div class="modalLevi">
+      <span class="close">&times;</span>
+      <p>That sounds like something Levi would do...</p>
+    </div>
+  
+  </div>`);
   } else {
     if (getFinalResult() === 'erinFinal') {
       finalResultEren();
@@ -212,4 +352,7 @@ $(function () {
   handlePageTurn();
   handleReset();
   handleAnswer();
+  modalExit();
+  currentScore();
+  handleStart();
 });
